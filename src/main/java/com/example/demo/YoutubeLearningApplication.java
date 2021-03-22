b76adda43a51410bf16ff843b0637657593d0758
mistrykamal <kamal.mistry@mavs.uta.edu>
2021-03-22 16:15:12 -0500

    an Example of RestTemplate to consume an api

--- src/main/java/com/example/demo/YoutubeLearningApplication.java
+++ src/main/java/com/example/demo/YoutubeLearningApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.example.demo.controller.EmployeesController;
+import com.example.demo.model.Employee;
 
 @SpringBootApplication
 public class YoutubeLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeLearningApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/demo/aspectorientprogramming/AspectConfig.java
+++ src/main/java/com/example/demo/aspectorientprogramming/AspectConfig.java
@@ -2,13 +2,17 @@ package com.example.demo.aspectorientprogramming;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
